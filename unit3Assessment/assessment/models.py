d161305c326272da2bbf18cd199e0081eeedc1b6
yiren <wang_yiren37@hotmail.com>
2020-11-20 15:37:52 -0500

    CRUD

--- unit3Assessment/assessment/models.py
+++ unit3Assessment/assessment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    description = models.CharField(max_length=200)
+    time = models.IntegerField()
+    person = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.description
--- unit3Assessment/assessment/views.py
+++ unit3Assessment/assessment/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from django.forms import ModelForm
 
 # Create your views here.
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+def home(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    return render(request, 'home.html', {
+        'task_list': tasks,
+        'form': form,
+    })
+
+def add(request):
+    form = TaskForm(request.POST)
+    if form.is_valid:
+        new_task = form.save(commit=False)
+        new_task.save()
+    return redirect('/')
+
+
+def delete(request, task_id):
+    Task.objects.get(id=task_id).delete()
+    return redirect('/')
